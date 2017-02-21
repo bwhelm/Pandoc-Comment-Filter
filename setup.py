060eba1075351a78d25f63ea7418b1555d47eaf9
Daven Quinn <dev@davenquinn.com>
2017-02-21 13:47:19 -0800

    Made pip-installable

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+install_requires = ['pandocfilters']
+
+setup(
+    name="Pandoc-Comment-Filter",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=install_requires,
+    include_package_data=True,
+    zip_safe=True,
+    entry_points="""
+        [console_scripts]
+        pandoc-comments=pandocCommentFilter:cli
+    """
+)
